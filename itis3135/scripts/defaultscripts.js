e6e930dd322325e9eba0460d613e45179f4c8869
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-02-10 18:49:11 -0500

    js update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,3 +2,10 @@ function scriptTest()
 {
     alert("My script is running.");
 }
+
+function formOutput(name, feeling)
+{
+    let name = document.getElementById("firstname");
+    let feeling = document.getElementById("feeling");
+    document.write("<h3>Hello, " + name + ".  We at Ruby Jackal are glad you are feeling " + feeling + ".</h3>");
+}
