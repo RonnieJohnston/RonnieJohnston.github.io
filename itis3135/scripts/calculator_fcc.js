6f2da6f82756aebdd734e711d093ebfa30d3ee5b
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-02-27 21:33:58 -0500

    Update calculator_fcc.js

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -2,6 +2,7 @@ window.onload = function()
 {
     const calculator = document.querySelector('.calculator');
     const keys = calculator.querySelector('.calculatorkeys');
+    const display = document.querySelector('.calculatordisplay');
 
     keys.addEventListener('click', e => 
     {
@@ -9,9 +10,21 @@ window.onload = function()
         {
             const key = e.target;
             const action = key.dataset.action;
+            const keyContent = key.textContent;
+            const displayedNum = display.textContent;
+
+            Array.from(key.parentNode.children)
+                .forEach(k => k.classList.remove('is-depressed'));
 
             if(!action)
             {
+                if(displayedNum === '0' || previousKeyType === 'operator')
+                {
+                    display.textContent = keyContent;
+                } else
+                {
+                    display.textContent = displayedNum + keyContent;
+                }
                 console.log('number key!');
             }
 
@@ -20,11 +33,16 @@ window.onload = function()
                 action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide'
             )
             {
+                key.classList.add('is-depressed');
+                calculator.dataset.previousKeyType = 'operator';
+                calculator.dataset.firstValue = displayedNum;
+                calculator.dataset.operator = action;
                 console.log('operator key!');
             }
 
             if(action === 'decimal')
             {
+                display.textContent = displayedNum + '.';
                 console.log('decimal key!');
             }
 
@@ -35,8 +53,36 @@ window.onload = function()
 
             if(action === 'calculate')
             {
+                const firstValue = calculator.datset.firstValue;
+                const operator = calculator.dataset.operator;
+                const secondValue = displayedNum;
+
+                display.textContent = calculate(firstValue, operator, secondValue);
                 console.log('equal key!');
             }
+        }
+
+        const calculate = (n1, operator, n2) =>
+        {
+            let result = '';
+
+            if (operator === 'add')
+            {
+                result = parseFloat(n1) + parseFloat(n2);
+
+            } else if (operator === 'subtract')
+            {
+                result = parseFloat(n1) - parseFloat(n2);
+
+            } else if (operator === 'multiply')
+            {
+                result = parseFloat(n1) * parseFloat(n2);
+
+            } else if (operator === 'divide')
+            {
+                result = parseFloat(n1) / parseFloat(n2);
+            }
+            return result;
         }
     })
 }
