0bcb14a507a2825e026df8788ac8047252639e37
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-02-27 23:01:45 -0500

    Update calculator_fcc.js

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -41,7 +41,7 @@ window.onload = function()
 
             if(!action)
             {
-                if(displayedNum === '0' || previousKeyType === 'operator')
+                if(displayedNum === '0' || previousKeyType === 'operator' || previousKeyType == 'calculate')
                 {
                     display.textContent = keyContent;
                 } else
@@ -57,9 +57,22 @@ window.onload = function()
                 action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide'
             )
             {
+                const firstValue = calculator.dataset.firstValue;
+                const operator = calculator.dataset.operator;
+                const secondValue = displayedNum;
+
+                if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate')
+                {
+                    const calcValue = calculate(firstValue, operator, secondValue);
+                    display.textContent = calcValue;
+                    calculator.dataset.firstValue = calcValue;
+                } else
+                {
+                    calculator.datset.firstValue = displayedNum;
+                }
+
                 key.classList.add('is-depressed');
                 calculator.dataset.previousKeyType = 'operator';
-                calculator.dataset.firstValue = displayedNum;
                 calculator.dataset.operator = action;
                 console.log('operator key!');
             }
@@ -69,7 +82,7 @@ window.onload = function()
                 if(!displayedNum.includes('.'))
                 {
                     display.textContent = displayedNum + '.';
-                } else if (previousKeyType === 'operator')
+                } else if (previousKeyType === 'operator' || previousKeyType == 'calculate')
                 {
                     display.textContent = '0.';
                 }
@@ -79,16 +92,44 @@ window.onload = function()
 
             if(action === 'clear')
             {
+                if (key.textContent === 'AC')
+                {
+                    calculator.dataset.firstValue = '';
+                    calculator.dataset.modValue = '';
+                    calculator.dataset.operator = '';
+                    calculator.dataset.previousKeyType = '';
+                } else
+                {
+                    key.textContent = 'AC';
+                }
+
+                display.textContent = 0;
                 calculator.dataset.previousKeyType = 'clear';
                 console.log('clear key!');
             }
 
+            if(action !== 'clear')
+            {
+                const clearButton = calculator.querySelector('[data-action=clear]');
+                clearButton.textContent = 'CE';
+            }
+
             if(action === 'calculate')
             {
-                const firstValue = calculator.dataset.firstValue;
+                let firstValue = calculator.dataset.firstValue;
                 const operator = calculator.dataset.operator;
-                const secondValue = displayedNum;
-                display.textContent = calculate(firstValue, operator, secondValue);
+                let secondValue = displayedNum;
+
+                if (firstValue) 
+                {
+                    if (previousKeyType === 'calculate')
+                    {
+                        firstValue = displayedNum;
+                        secondValue = calculator.dataset.modValue;
+                    }
+                    display.textContent = calculate(firstValue, operator, secondValue);
+                }
+                calculator.dataset.modValue = secondValue;
                 calculator.dataset.previousKeyType = 'calculate';
                 console.log('equal key!');
             }
