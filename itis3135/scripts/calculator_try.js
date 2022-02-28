d9f6fb6555c6f87768c4c5e8b8f061f629192114
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-02-27 20:48:59 -0500

    calculator try update

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -16,3 +16,16 @@ function appendNum(inputNum)
         document.getElementById('output').innerHTML = outputString;
     }
 }
+
+/* Function to ensure buttons do something */
+function testAlert(num)
+{
+    alert("You have entered: " + num);
+}
+
+/* Uncaught TypeError: Cannot read properties of null (reading 'appendNum')
+    at appendNum (calculator_try.js:11:22)
+    at HTMLButtonElement.onclick (calculator_try.html:50:75)
+
+    Unsure why this is happening, moving on to tutorial.
+*/
