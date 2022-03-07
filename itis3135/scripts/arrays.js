977fc3fda5bc5285e1db1fccefd8240cd08a4caf
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-03-07 16:42:48 -0500

    arrays update

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -8,7 +8,14 @@ function addSalary()
 
 function displayResults()
 {
-
+    let sum = 0;
+    let avg = 0;
+    for(let i = 0; i < salaryArray.length; i++)
+    {
+        let temp = parseFloat(salaryArray[i]).toFixed(2);
+        sum += temp;
+    }
+    avg = (sum / salaryArray.length)
 }
 
 function displaySalary()
