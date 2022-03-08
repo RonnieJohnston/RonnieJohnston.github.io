3a11947eb931577dfb7f4d0fb9c77494de758412
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-03-07 21:07:07 -0500

    array update

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,13 +1,39 @@
 let personArray = new Array();
 let salaryArray = new Array();
-let employeeTableBody = document.getElementById('employeetablebody');
+
+let $ = function(id)
+{
+    return document.getElementById(id);
+}
 
 function addSalary()
 {
+    if($('employeename').value === "" || $('employeesalary').value === "")
+    {
+        alert("Please enter required values.");
+        return;
+    } else if (isNaN($('employeesalary').value) || $('employeesalary').value < 0)
+    {
+        alert("Please enter a valid number in the salary field")
+        return;
+    }
 
+    let name = $('employeename').value;
+    let salary = parseFloat($('employeesalary').value).toFixed(2);
+
+    personArray.push(name);
+    salaryArray.push(salary);
 }
 
 function displayResults()
+{
+    let avg = calcAvg();
+    let highestString = calcHighest();
+
+    $('results').innerHTML = ("The average salary of the table is: $" + avg + "<br />" + highestString);
+}
+
+function calcAvg()
 {
     let sum = 0;
     let avg = 0;
@@ -16,13 +42,35 @@ function displayResults()
         let temp = parseFloat(salaryArray[i]).toFixed(2);
         sum += temp;
     }
-    avg = (sum / salaryArray.length)
+    avg = (sum / salaryArray.length);
+    return avg;
+}
+
+function calcHighest()
+{
+    let highestSalary = 0;
+    let highestPerson = null;
+    for(let i = 0; i < salaryArray.length; i++)
+    {
+        if(salaryArray[i] > highest)
+        {
+            highestSalary = salaryArray[i];
+            highestPerson = personArray[i];
+        }
+    }
+
+    return ("The person with the highest salary is " + highestPerson + " at $" + highestSalary + ".");
 }
 
 function displaySalary()
 {
+    $('#employeetablebody').remove();
     for(let i = 0; i < personArray.length; i++)
     {
-        let row = employeeTable.inser
+        let row = $('#employeetablebody').insertRow(i);
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        cell0.innerHTML = personArray[i];
+        cell1.innerHTML = salaryArray[i];
     }
 }
