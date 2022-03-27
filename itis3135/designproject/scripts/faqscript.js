3947a3fe03d534723a1050fbec2ee52f727ed39f
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-03-27 18:29:08 -0400

    Update faqscript.js

--- itis3135/designproject/scripts/faqscript.js
+++ itis3135/designproject/scripts/faqscript.js
@@ -5,11 +5,16 @@ let $ = function(id)
 
 function toggleStructureContent()
 {
-	if($('structure-content').style.display === auto)
+	let element = $('structure-content');
+  let style = window.getComputedStyle(element);
+  let display = style.getPropertyValue('display');
+  console.log(display);
+  
+	if(display === 'block')
   {
-  	$('structure-content').style.display = none;
-  } else if($('structure-content').style.display === none)
+  	$('structure-content').style.display = 'none';
+  } else if(display === 'none')
   {
-  	$('structure-content').style.display = auto;
+  	$('structure-content').style.display = 'block';
   }
 }
