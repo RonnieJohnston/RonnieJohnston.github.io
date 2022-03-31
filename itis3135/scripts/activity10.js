668bf1340c0c7c4e58c362799b93f4806124205b
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-03-31 18:31:22 -0400

    Update activity10.js

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -11,8 +11,10 @@ $(document).ready(function() {
         const link = evt.currentTarget;
         $("#image").fadeOut(500, function() {
             $("#image").attr("src", link.href);
-            $("#image").fadeIn(1000);
+            $("#image").fadeIn(500);
+            $("#caption").fadeOut(500);
             $("#caption").text(link.title);
+            $("#caption").fadeIn(500);
         })
         // prevent default action
         evt.preventDefault();
