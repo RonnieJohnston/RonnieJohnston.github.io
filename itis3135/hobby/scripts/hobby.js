086ffbfc8821510aa27c31e7eb1f6db922c6e625
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-04-02 02:54:47 -0400

    hobby js

---
+++ itis3135/hobby/scripts/hobby.js
@@ -0,0 +1,47 @@
+function $(id)
+{
+	return document.getElementById(id);
+}
+
+window.onclick = function(evt)
+{
+	if(evt.target == $("home-button"))
+  {
+  	$("home-page-content").style.display = "auto";
+  } else if(evt.target == $("systems-button") || evt.target == $("favorites-button") || evt.target == $("wishlist-button") || evt.target == $("about-button"))
+  {
+  	$("home-page-content").style.display = "none";
+  }
+  
+  if(evt.target == $("systems-button"))
+  {
+  	$("systems-page-content").style.display = "auto";
+  } else if(evt.target == $("home-button") || evt.target == $("favorites-button") || evt.target == $("wishlist-button") || evt.target == $("about-button"))
+  {
+  	$("systems-page-content").style.display = "none";
+  }
+  
+  if(evt.target == $("favorites-button"))
+  {
+  	$("favorites-page-content").style.display = "auto";
+  } else if(evt.target == $("systems-button") || evt.target == $("home-button") || evt.target == $("wishlist-button") || evt.target == $("about-button"))
+  {
+  	$("favorites-page-content").style.display = "none";
+  }
+  
+  if(evt.target == $("wishlist-button"))
+  {
+  	$("wishlist-page-content").style.display = "auto";
+  } else if(evt.target == $("systems-button") || evt.target == $("favorites-button") || evt.target == $("home-button") || evt.target == $("about-button"))
+  {
+  	$("wishlist-page-content").style.display = "none";
+  }
+  
+  if(evt.target == $("about-button"))
+  {
+  	$("about-page-content").style.display = "auto";
+  } else if(evt.target == $("systems-button") || evt.target == $("favorites-button") || evt.target == $("wishlist-button") || evt.target == $("home-button"))
+  {
+  	$("about-page-content").style.display = "none";
+  }
+}
