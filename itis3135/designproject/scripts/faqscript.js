71d4b588dddfc10d31721b08a55f3904984873d7
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-04-09 19:35:00 -0400

    accordion for faq

--- itis3135/designproject/scripts/faqscript.js
+++ itis3135/designproject/scripts/faqscript.js
@@ -1,20 +1,8 @@
-let $ = function(id)
-{
-	return document.getElementById(id);
-}
-
-function toggleStructureContent()
-{
-	let element = $('structure-content');
-  let style = window.getComputedStyle(element);
-  let display = style.getPropertyValue('display');
-  console.log(display);
-  
-	if(display === 'block')
-  {
-  	$('structure-content').style.display = 'none';
-  } else if(display === 'none')
-  {
-  	$('structure-content').style.display = 'block';
-  }
-}
+$(document).ready( () =>
+    $("#panels").accordion(
+        {
+            heightStyle: "content",
+            collapsible: true,
+            active: false
+        })
+)
