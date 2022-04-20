c4cfea2340784bb6a35c56966e0f021be19ce77a
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-04-20 15:22:54 -0400

    Update Johnston-Activity14.js

--- itis3135/scripts/Johnston-Activity14.js
+++ itis3135/scripts/Johnston-Activity14.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    
+    // set onclick functions
+    $("a").click(function() {
+        // fetch respective json file
+        getJSON($(this).attr('title') + ".json");
+    })
 }); // end ready
+
+// locate and display data from json file
+function getJSON(file) {
+    $.ajax( {
+        //  use parameter for filepath
+        url: file,
+        dataType: "Text",
+        success: function(file) {
+            // set html elements to display data
+            let data = $.parseJSON(file);
+            $("main > h1").html(data.speakers[0].title);
+            $("main > h2").html(data.speakers[0].month);
+            $("main > h3").html(data.speakers[0].speaker);
+            $("main > img").attr("src", data.speakers[0].image);
+            $("main > p").html(data.speakers[0].text);
+        }
+    })
+}
