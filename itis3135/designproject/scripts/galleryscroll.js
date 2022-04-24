d82d86352af64e65ea6dd90114f4c42d8ccc4118
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-04-24 17:50:09 -0400

    scroll plugin

---
+++ itis3135/designproject/scripts/galleryscroll.js
@@ -0,0 +1,9 @@
+$(document).ready( () =>
+    $(".main").tiltedpage_scroll({
+        sectionContainer: "> section",     
+        angle: 50,                         
+        opacity: true,                     
+        scale: true,                       
+        outAnimation: true  
+    })
+)
