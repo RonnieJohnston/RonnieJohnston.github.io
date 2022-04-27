718451cab7b0afb082e23da25a53e817db85007d
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2022-04-26 21:38:28 -0400

    activity 15

--- itis3135/scripts/Johnston-Activity15.js
+++ itis3135/scripts/Johnston-Activity15.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+    $.getJSON("facultyList.json",function(data) {
+        $.each(data, function() {
+            $.each(this, function(value) {
+                $("#faculty").append(
+                    "<img src=" + "'" + value.image + "'>" +
+                    "<h2>" + value.full_name + "</h2>" +
+                    "<h3>" + value.department + "<br><p>" + value.bio + "</p></h3>"
+                )
+            })
+        })
+    })
 });
