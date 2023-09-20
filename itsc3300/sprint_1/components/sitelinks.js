e6594acb34c13c81125b1e5062b1144ccf6e0d7a
parth448812 <parthkpatel04@gmail.com>
2023-09-19 20:48:59 -0400

    Quick Updates

--- itsc3300/sprint_1/components/sitelinks.js
+++ itsc3300/sprint_1/components/sitelinks.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.createElement("header");
     header.innerHTML = `
-    <h1 class = "cent">Sprint0 blogs</h1>
+    <h1 class = "cent">Sprint1 blogs</h1>
     <nav>
         <a href="index.html">Stories</a> |
         <a href="parth_blog_sprint1.html">Parth's&nbsp;Blog</a> |
