9ca87eb1c05540fe5ebd68018de0a61c6cd0d145
parth448812 <parthkpatel04@gmail.com>
2023-09-19 20:43:52 -0400

    Foundation for Sprint 1

--- itsc3300/scripts/siteLinks.js
+++ itsc3300/scripts/siteLinks.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
           Agile Project Management
       </h1>
       <nav>
-        <a href="sprint_0/index.html">Sprint0 Blogs</a> 
+        <a href="sprint_0/index.html">Sprint0 Blogs</a> |
+        <a href="sprint_1/index.html">Sprint1 Blogs</a> 
       </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
