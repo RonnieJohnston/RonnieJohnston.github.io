8342d17206e2c5fc54688fb2bd5191470daaf32b
parth448812 <parthkpatel04@gmail.com>
2023-10-17 19:46:10 -0400

    Sp3 Wk1 SetUp

--- itsc3300/scripts/siteLinks.js
+++ itsc3300/scripts/siteLinks.js
@@ -6,10 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
       </h1>
       <nav>
         <a href="sprint_0/index.html">Sprint0 Blogs</a> |
-        <a href="sprint_1/index.html">Sprint1 Blogs</a> |
+        <a href="sprint_1/index.html">Sprint1 Week 1 Blogs</a> |
         <a href="sprint_1_week2/index.html">Sprint1 Week 2 Blogs</a> |
         <a href="sprint_2_week1/index.html">Sprint2 Week 1 Blogs</a> |
-        <a href="sprint_2_week2/index.html">Sprint2 Week 2 Blogs</a>
+        <a href="sprint_2_week2/index.html">Sprint2 Week 2 Blogs</a> |
+        <a href="sprint_3_week1/index.html">Sprint3 Week 1 Blogs</a>
       </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
