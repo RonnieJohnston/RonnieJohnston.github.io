0a792079418c201eeab0f4a52adea33cc9bf6558
parth448812 <parthkpatel04@gmail.com>
2023-10-23 16:25:31 -0400

    Sp3 wk2 Site Setup and Parth Sp3 wk2 Blog Upload

--- itsc3300/scripts/siteLinks.js
+++ itsc3300/scripts/siteLinks.js
@@ -10,7 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
         <a href="sprint_1_week2/index.html">Sprint1 Week 2 Blogs</a> |
         <a href="sprint_2_week1/index.html">Sprint2 Week 1 Blogs</a> |
         <a href="sprint_2_week2/index.html">Sprint2 Week 2 Blogs</a> |
-        <a href="sprint_3_week1/index.html">Sprint3 Week 1 Blogs</a>
+        <a href="sprint_3_week1/index.html">Sprint3 Week 1 Blogs</a> |
+        <a href="sprint_3_week2/index.html">Sprint3 Week 2 Blogs</a>
       </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
