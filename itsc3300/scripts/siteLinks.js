7321763a24e87d6b6b461d46f80f536a4831c7cb
parth448812 <parthkpatel04@gmail.com>
2023-12-02 16:55:58 -0500

    navigation bar update

--- itsc3300/scripts/siteLinks.js
+++ itsc3300/scripts/siteLinks.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
           Agile Project Management
       </h1>
       <nav>
-        <a href="sprint_0/index.html">Sprint0 Blogs</a> |
-        <a href="sprint_1/index.html">Sprint1 Week 1 Blogs</a> |
-        <a href="sprint_1_week2/index.html">Sprint1 Week 2 Blogs</a> |
-        <a href="sprint_2_week1/index.html">Sprint2 Week 1 Blogs</a> |
-        <a href="sprint_2_week2/index.html">Sprint2 Week 2 Blogs</a> |
-        <a href="sprint_3_week1/index.html">Sprint3 Week 1 Blogs</a> |
-        <a href="sprint_3_week2/index.html">Sprint3 Week 2 Blogs</a> |
-        <a href="sprint_4_week1/index.html">Sprint4 Week 1 Blogs</a> |
-        <a href="sprint_4_week2/index.html">Sprint4 Week 2 Blogs</a>
+        <a href="sprint_0/index.html">Agile&nbspMangement</a> |
+        <a href="sprint_1/index.html">Scope&nbspMangement</a> |
+        <a href="sprint_1_week2/index.html">Schedule&nbspMangement</a> |
+        <a href="sprint_2_week1/index.html">Cost&nbspMangement</a> |
+        <a href="sprint_2_week2/index.html">Quality&nbspMangement</a> |
+        <a href="sprint_3_week1/index.html">Resource&nbspMangement</a> |
+        <a href="sprint_3_week2/index.html">Communications&nbspMangement</a> |
+        <a href="sprint_4_week1/index.html">Risk&nbspMangement</a> |
+        <a href="sprint_4_week2/index.html">Procurement&nbspMangement</a>
       </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
