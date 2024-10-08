9e4eb7c30a6e2f5f82c3109ba83ba9de75811fae
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2024-10-08 15:48:45 -0400

    Update skills_script.js

--- mainpage/scripts/skills_script.js
+++ mainpage/scripts/skills_script.js
@@ -1,18 +1,13 @@
-const $ = function(id) 
-{
-    return document.getElementById(id);
-}
-
-document.ready(function() {
+$(document).ready(function() {
     let skills = [["JavaScript", "javascript.png"], ["HTML", "html.png"], ["CSS", "css.png"], ["C#", "csharp.png"], ["Python", "python.png"], ["GitHub", "github.png"],
  ["Trello", null], ["REST API", "rest.png"], ["ReactJS", "react.png"], ["Agile Development", null], ["MySQL", null]];
 
     for(entry in skills) {
         console.log("TEST");
         if(entry[1] !== null) {
-            $("skills_list").innerHTML += "<p class='skill-item'>" + entry[0] + "<img class='skill-image' src='" + entry[1] +"></p>" 
+            $("#skills_list").append("<p class='skill-item'>" + entry[0] + "<img class='skill-image' src='" + entry[1] +"></p>");
         } else {
-            $("skills_list").innerHTML += "<p class='skill-item'>" + entry[0] + "</p>"
+            $("#skills_list").append("<p class='skill-item'>" + entry[0] + "</p>");
         }
     }
 })
