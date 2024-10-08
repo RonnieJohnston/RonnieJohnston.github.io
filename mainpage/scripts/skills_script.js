c4f5f91413a0c5115d40b8a1377771a67b32aa31
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2024-10-07 23:14:01 -0400

    Update skills_script.js

--- mainpage/scripts/skills_script.js
+++ mainpage/scripts/skills_script.js
@@ -1,12 +1,7 @@
-const $ = function(id) 
-{
-    return document.getElementById(id);
-}
-
-let skills = [["JavaScript", "javascript.png"], ["HTML", "html.png"], ["CSS", "css.png"], ["C#", "csharp.png"], ["Python", "python.png"], ["GitHub", "github.png"],
- ["Trello", null], ["REST API", "rest.png"], ["ReactJS", "react.png"], ["Agile Development", null], ["MySQL", null]];
- 
 $(document).ready(function() {
+    let skills = [["JavaScript", "javascript.png"], ["HTML", "html.png"], ["CSS", "css.png"], ["C#", "csharp.png"], ["Python", "python.png"], ["GitHub", "github.png"],
+ ["Trello", null], ["REST API", "rest.png"], ["ReactJS", "react.png"], ["Agile Development", null], ["MySQL", null]];
+
     for(entry in skills) {
         setTimeout(function() {
             if(entry[1] !== null) {
