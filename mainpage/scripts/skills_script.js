d96c0141fdfe937f1caa9583145cb80b14bcf024
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2024-10-08 16:15:09 -0400

    Update skills_script.js

--- mainpage/scripts/skills_script.js
+++ mainpage/scripts/skills_script.js
@@ -1,13 +1,14 @@
-//$(document).ready(function() {
-    let skills = [["JavaScript", "javascript.png"], ["HTML", "html.png"], ["CSS", "css.png"], ["C#", "csharp.png"], ["Python", "python.png"], ["GitHub", "github.png"],
- ["Trello", null], ["REST API", "rest.png"], ["ReactJS", "react.png"], ["Agile Development", null], ["MySQL", null]];
+$(document).ready(function() {
+    let skills = [["JavaScript", "images/javascript.png"], ["HTML", "images/html.png"], ["CSS", "images/css.png"], ["C#", "images/csharp.png"], 
+    ["Python", "images/python.png"], ["GitHub", "images/github.png"], ["Trello", null], ["REST API", "images/rest.png"], ["ReactJS", "images/react.png"], 
+    ["Agile Development", null], ["MySQL", null]];
 
     for(entry in skills) {
-        console.log(skills[entry]);
+        console.log(skills[entry][1]);
         if(skills[entry][1] !== null) {
             $("#skills_list").append("<p class='skill-item'>" + skills[entry][0] + "<img class='skill-image' src='" + skills[entry][1] +"></p>");
         } else {
             $("#skills_list").append("<p class='skill-item'>" + skills[entry][0] + "</p>");
         }
     }
-//})
+})
