d89197dd20b78e3d5c376bddda09ca79d456fd69
RonnieJohnston <97621743+RonnieJohnston@users.noreply.github.com>
2024-10-08 16:20:02 -0400

    Update skills_script.js

--- mainpage/scripts/skills_script.js
+++ mainpage/scripts/skills_script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     for(entry in skills) {
         if(skills[entry][1] !== null) {
-            $("#skills_list").append("<p class='skill-item'>" + skills[entry][0] + "<img class='skill-image' src='" + skills[entry][1] +"></p>");
+            $("#skills_list").append("<p class='skill-item'>" + skills[entry][0] + "<img class='skill-image' src='" + skills[entry][1] +"'></p>");
         } else {
             console.log(skills[entry][1]);
             $("#skills_list").append("<p class='skill-item'>" + skills[entry][0] + "</p>");
